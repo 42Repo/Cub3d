869b7985e4cbc58bdb64f3f313a8bdfa748871a4
RenHartt <baptiste.goron@gmail.com>
2024-06-24 23:52:25 +0200

    norme in progress rotate_minimap

--- bonus/src/minimap/rotate_minimap.c
+++ bonus/src/minimap/rotate_minimap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotate_minimap.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asuc <asuc@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: bgoron <bgoron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 22:37:15 by asuc              #+#    #+#             */
-/*   Updated: 2024/06/21 18:53:03 by asuc             ###   ########.fr       */
+/*   Updated: 2024/06/24 23:48:11 by bgoron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,70 +96,87 @@ void	draw_player_marker(t_data *data, int center_x, int center_y)
 
 void	draw_minimap(t_data *data)
 {
-	int minimap_size = (2 * (((WIN_HEIGHT * 0.28 / 2 ) / 1.5))) * data->settings.minimap_scale;
+	t_vec2_int	pos;
+	t_vec2		player_map;
+	t_vec2_int	d;
+	t_vec2_int	rotated;
+	t_vec2		final;
+	t_vec2_int	map;
+	t_vec2_int	window_offset;
+	float		angle;
+	float 		cos_angle;
+	float 		sin_angle;
+	int 		minimap_size;
+	minimap_size = (2 * (((WIN_HEIGHT * 0.28 / 2 ) / 1.5))) * data->settings.minimap_scale;
 	int mask[minimap_size * minimap_size];
 	create_circular_mask(mask, minimap_size, minimap_size);
 
-	float player_map_x = data->player.pos.x;
-	float player_map_y = data->player.pos.y;
-
-	for (int y = 0; y < minimap_size; y++)
+	player_map.x = data->player.pos.x;
+	player_map.y = data->player.pos.y;
+	pos.y = 0;
+	while (pos.y < minimap_size)
 	{
-		for (int x = 0; x < minimap_size; x++)
+		pos.x = 0;
+		while (pos.x < minimap_size)
 		{
-			mlx_set_image_pixel(data->graphics.mlx, data->graphics.img_mini_map, x, y,
+			mlx_set_image_pixel(data->graphics.mlx, data->graphics.img_mini_map, pos.x, pos.y,
 				0x00000000);
+			pos.x++;
 		}
+		pos.y++;
 	}
-	float angle = atan2(data->player.dir.y, data->player.dir.x);
-	float cos_angle = cos(angle);
-	float sin_angle = sin(angle);
-
-	for (int y = 0; y < minimap_size; y++)
+	angle = atan2(data->player.dir.y, data->player.dir.x);
+	cos_angle = cos(angle);
+	sin_angle = sin(angle);
+	pos.y = 0;
+	while (pos.y < minimap_size)
 	{
-		for (int x = 0; x < minimap_size; x++)
+		pos.x = 0;
+		while (pos.x < minimap_size)
 		{
-			if (mask[y * minimap_size + x])
+			if (mask[pos.y * minimap_size + pos.x])
 			{
-				int dx = x - (((WIN_HEIGHT * 0.28 / 2 ) / 1.5)) * data->settings.minimap_scale;
-				int dy = y - (((WIN_HEIGHT * 0.28 / 2 ) / 1.5)) * data->settings.minimap_scale;
-				int rotated_x = -dy;
-				int rotated_y = dx;
-				float final_x = cos_angle * rotated_x - sin_angle * rotated_y;
-				float final_y = sin_angle * rotated_x + cos_angle * rotated_y;
-				int map_x = player_map_x + final_x / MINIMAP_SCALE * data->settings.minimap_scale;
-				int map_y = player_map_y + final_y / MINIMAP_SCALE * data->settings.minimap_scale;
+				d.x = pos.x - (((WIN_HEIGHT * 0.28 / 2 ) / 1.5)) * data->settings.minimap_scale;
+				d.y = pos.y - (((WIN_HEIGHT * 0.28 / 2 ) / 1.5)) * data->settings.minimap_scale;
+				rotated.x = -d.y;
+				rotated.y = d.x;
+				final.x = cos_angle * rotated.x - sin_angle * rotated.y;
+				final.y = sin_angle * rotated.x + cos_angle * rotated.y;
+				map.x = player_map.x + final.x / MINIMAP_SCALE * data->settings.minimap_scale;
+				map.y = player_map.y + final.y / MINIMAP_SCALE * data->settings.minimap_scale;
 
 				int color = 0xFF808080;
-				if (map_x >= 0 && map_x < data->map.cols && map_y >= 0
-					&& map_y < data->map.rows)
+				if (map.x >= 0 && map.x < data->map.cols && map.y >= 0
+					&& map.y < data->map.rows)
 				{
-					if (data->map.map[map_y][map_x] == '1')
-					{
+					if (data->map.map[map.y][map.x] == '1')
 						color = 0xFFFF0000;
-					}
-					if (data->map.map[map_y][map_x] == 'D')
-					{
+					if (data->map.map[map.y][map.x] == 'D')
 						color = 0xFF0000FF;
-					}
 				}
-				mlx_set_image_pixel(data->graphics.mlx, data->graphics.img_mini_map, x, y,
+				mlx_set_image_pixel(data->graphics.mlx, data->graphics.img_mini_map, pos.x, pos.y,
 					color);
 			}
+			pos.x++;
 		}
+		pos.y++;
 	}
 	draw_player_marker(data, (((WIN_HEIGHT * 0.28 / 2 ) / 1.5)) * data->settings.minimap_scale, (((WIN_HEIGHT * 0.28 / 2 ) / 1.5)) * data->settings.minimap_scale);
-	int window_offset_x = 10;
-	int window_offset_y = 10;
-	for (int y = 0; y < minimap_size; y++)
+	window_offset.x = 10;
+	window_offset.y = 10;
+	pos.y = 0;
+	while (pos.y < minimap_size)
 	{
-		for (int x = 0; x < minimap_size; x++)
+		pos.x = 0;
+		while (pos.x < minimap_size)
 		{
 			int color = mlx_get_image_pixel(data->graphics.mlx,
-					data->graphics.img_mini_map, x, y);
+					data->graphics.img_mini_map, pos.x, pos.y);
 			if (color != 0x00000000)
-				mlx_pixel_put(data->graphics.mlx, data->graphics.win, window_offset_x + x,
-					window_offset_y + y, color);
+				mlx_pixel_put(data->graphics.mlx, data->graphics.win, window_offset.x + pos.x,
+					window_offset.y + pos.y, color);
+			pos.x++;
 		}
+		pos.y++;
 	}
 }
