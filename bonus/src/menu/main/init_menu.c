6560a814b0d7c26e6bc1769dbc10451e0ca9c999
RenHartt <baptiste.goron@gmail.com>
2024-06-28 17:47:29 +0200

    norme en cours encore et toujours

--- bonus/src/menu/main/init_menu.c
+++ bonus/src/menu/main/init_menu.c
@@ -6,7 +6,7 @@
 /*   By: bgoron <bgoron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 21:41:16 by asuc              #+#    #+#             */
-/*   Updated: 2024/06/27 14:14:00 by bgoron           ###   ########.fr       */
+/*   Updated: 2024/06/28 17:47:07 by bgoron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,58 +48,70 @@ static void	resize_image(t_data *data, t_image *src, t_vec2i new_size)
 	*src = dst;
 }
 
+void	load_menu_image(t_data *data, t_menu *m)
+{
+	m->img_background.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/menu_background.png", &m->img_background.width, &m->img_background.height);
+	m->img_background_button.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/GUI.png", &m->img_background_button.width, &m->img_background_button.height);
+	m->play_button.img.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/start.png", &m->play_button.img.width, &m->play_button.img.height);
+	m->play_button.img_hovered.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/start_hovered.png", &m->play_button.img_hovered.width, &m->play_button.img_hovered.height);
+	m->play_button.img_pressed.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/start_pressed.png", &m->play_button.img_pressed.width, &m->play_button.img_pressed.height);
+	m->exit_button.img.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/exit.png", &m->exit_button.img.width, &m->exit_button.img.height);
+	m->exit_button.img_hovered.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/exit_hovered.png", &m->exit_button.img_hovered.width, &m->exit_button.img_hovered.height);
+	m->exit_button.img_pressed.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/exit_pressed.png", &m->exit_button.img_pressed.width, &m->exit_button.img_pressed.height);
+	m->settings_button.img.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/settings.png", &m->settings_button.img.width, &m->settings_button.img.height);
+	m->settings_button.img_hovered.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/settings_hovered.png", &m->settings_button.img_hovered.width, &m->settings_button.img_hovered.height);
+	m->settings_button.img_pressed.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/settings_pressed.png", &m->settings_button.img_pressed.width, &m->settings_button.img_pressed.height);
+}
+
+void	resize_menu_image(t_data *data, t_menu *m, t_vec2f scale_factor)
+{
+	t_vec2i	new_size;
+
+	new_size = (t_vec2i){scale_factor.x * 7, scale_factor.y * 7};
+	resize_image(data, &m->img_background, (t_vec2i){WIN_WIDTH, WIN_HEIGHT});
+	resize_image(data, &m->img_background_button, (t_vec2i){m->img_background_button.width * new_size.x, m->img_background_button.height * new_size.y});
+	resize_image(data, &m->play_button.img, (t_vec2i){m->play_button.img.width * new_size.x, m->play_button.img.height * new_size.y});
+	resize_image(data, &m->play_button.img_hovered, (t_vec2i){m->play_button.img_hovered.width * new_size.x, m->play_button.img_hovered.height * new_size.y});
+	resize_image(data, &m->play_button.img_pressed, (t_vec2i){m->play_button.img_pressed.width * new_size.x, m->play_button.img_pressed.height * new_size.y});
+	resize_image(data, &m->exit_button.img, (t_vec2i){m->exit_button.img.width * new_size.x, m->exit_button.img.height * new_size.y});
+	resize_image(data, &m->exit_button.img_hovered, (t_vec2i){m->exit_button.img_hovered.width * new_size.x, m->exit_button.img_hovered.height * new_size.y});
+	resize_image(data, &m->exit_button.img_pressed, (t_vec2i){m->exit_button.img_pressed.width * new_size.x, m->exit_button.img_pressed.height * new_size.y});
+	resize_image(data, &m->settings_button.img, (t_vec2i){m->settings_button.img.width * new_size.x, m->settings_button.img.height * new_size.y});
+	resize_image(data, &m->settings_button.img_hovered, (t_vec2i){m->settings_button.img_hovered.width * new_size.x, m->settings_button.img_hovered.height * new_size.y});
+	resize_image(data, &m->settings_button.img_pressed, (t_vec2i){m->settings_button.img_pressed.width * new_size.x, m->settings_button.img_pressed.height * new_size.y});
+}
+
+void	set_button_pos(t_menu *m, t_vec2f scale_factor)
+{
+	m->play_button.pos.x = (WIN_WIDTH / 2) - (m->img_background_button.width / 2) + 56 * scale_factor.x;
+	m->play_button.pos.y = (WIN_HEIGHT / 2) - (m->img_background_button.height / 2) + 77 * scale_factor.y;
+	m->play_button.size.x = m->play_button.img.width;
+	m->play_button.size.y = m->play_button.img.height;
+	m->settings_button.pos.x = m->play_button.pos.x;
+	m->settings_button.pos.y = m->play_button.pos.y + m->play_button.size.y + 14 * scale_factor.y;
+	m->settings_button.size.x = m->settings_button.img.width;
+	m->settings_button.size.y = m->settings_button.img.height;
+	m->exit_button.pos.x = m->settings_button.pos.x;
+	m->exit_button.pos.y = m->settings_button.pos.y + m->settings_button.size.y + 14 * scale_factor.y;
+	m->exit_button.size.x = m->exit_button.img.width;
+	m->exit_button.size.y = m->exit_button.img.height;
+}
+
 void	init_menu(t_data *data)
 {
-	float scale_factor_width = (float)WIN_WIDTH / 1920.0f;
-	float scale_factor_height = (float)WIN_HEIGHT / 1080.0f;
+	t_menu	*m;
+	t_vec2f	scale_factor;
 
-	data->graphics.menu.img_background.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/menu_background.png", &data->graphics.menu.img_background.width, &data->graphics.menu.img_background.height);
-	resize_image(data, &data->graphics.menu.img_background, (t_vec2i){WIN_WIDTH, WIN_HEIGHT});
-
-	data->graphics.menu.img_background_button.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/GUI.png", &data->graphics.menu.img_background_button.width, &data->graphics.menu.img_background_button.height);
-	resize_image(data, &data->graphics.menu.img_background_button, (t_vec2i){data->graphics.menu.img_background_button.width * scale_factor_width * 7, data->graphics.menu.img_background_button.height * scale_factor_height * 7});
-
-	data->graphics.menu.play_button.img.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/start.png", &data->graphics.menu.play_button.img.width, &data->graphics.menu.play_button.img.height);
-	resize_image(data, &data->graphics.menu.play_button.img, (t_vec2i){data->graphics.menu.play_button.img.width * scale_factor_width * 7, data->graphics.menu.play_button.img.height * scale_factor_height * 7});
-
-	data->graphics.menu.play_button.img_hovered.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/start_hovered.png", &data->graphics.menu.play_button.img_hovered.width, &data->graphics.menu.play_button.img_hovered.height);
-	resize_image(data, &data->graphics.menu.play_button.img_hovered, (t_vec2i){data->graphics.menu.play_button.img_hovered.width * scale_factor_width * 7, data->graphics.menu.play_button.img_hovered.height * scale_factor_height * 7});
-
-	data->graphics.menu.play_button.img_pressed.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/start_pressed.png", &data->graphics.menu.play_button.img_pressed.width, &data->graphics.menu.play_button.img_pressed.height);
-	resize_image(data, &data->graphics.menu.play_button.img_pressed, (t_vec2i){data->graphics.menu.play_button.img_pressed.width * scale_factor_width * 7, data->graphics.menu.play_button.img_pressed.height * scale_factor_height * 7});
-
-	data->graphics.menu.exit_button.img.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/exit.png", &data->graphics.menu.exit_button.img.width, &data->graphics.menu.exit_button.img.height);
-	resize_image(data, &data->graphics.menu.exit_button.img, (t_vec2i){data->graphics.menu.exit_button.img.width * scale_factor_width * 7, data->graphics.menu.exit_button.img.height * scale_factor_height * 7});
-
-	data->graphics.menu.exit_button.img_hovered.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/exit_hovered.png", &data->graphics.menu.exit_button.img_hovered.width, &data->graphics.menu.exit_button.img_hovered.height);
-	resize_image(data, &data->graphics.menu.exit_button.img_hovered, (t_vec2i){data->graphics.menu.exit_button.img_hovered.width * scale_factor_width * 7, data->graphics.menu.exit_button.img_hovered.height * scale_factor_height * 7});
-
-	data->graphics.menu.exit_button.img_pressed.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/exit_pressed.png", &data->graphics.menu.exit_button.img_pressed.width, &data->graphics.menu.exit_button.img_pressed.height);
-	resize_image(data, &data->graphics.menu.exit_button.img_pressed, (t_vec2i){data->graphics.menu.exit_button.img_pressed.width * scale_factor_width * 7, data->graphics.menu.exit_button.img_pressed.height * scale_factor_height * 7});
-
-	data->graphics.menu.settings_button.img.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/settings.png", &data->graphics.menu.settings_button.img.width, &data->graphics.menu.settings_button.img.height);
-	resize_image(data, &data->graphics.menu.settings_button.img, (t_vec2i){data->graphics.menu.settings_button.img.width * scale_factor_width * 7, data->graphics.menu.settings_button.img.height * scale_factor_height * 7});
-
-	data->graphics.menu.settings_button.img_hovered.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/settings_hovered.png", &data->graphics.menu.settings_button.img_hovered.width, &data->graphics.menu.settings_button.img_hovered.height);
-	resize_image(data, &data->graphics.menu.settings_button.img_hovered, (t_vec2i){data->graphics.menu.settings_button.img_hovered.width * scale_factor_width * 7, data->graphics.menu.settings_button.img_hovered.height * scale_factor_height * 7});
-
-	data->graphics.menu.settings_button.img_pressed.img = mlx_png_file_to_image(data->graphics.mlx, "textures/menu/settings_pressed.png", &data->graphics.menu.settings_button.img_pressed.width, &data->graphics.menu.settings_button.img_pressed.height);
-	resize_image(data, &data->graphics.menu.settings_button.img_pressed, (t_vec2i){data->graphics.menu.settings_button.img_pressed.width * scale_factor_width * 7, data->graphics.menu.settings_button.img_pressed.height * scale_factor_height * 7});
-
-	data->graphics.menu.play_button.pos = (t_vec2i){(WIN_WIDTH / 2) - (data->graphics.menu.img_background_button.width / 2) + 56 * scale_factor_width, (WIN_HEIGHT / 2) - (data->graphics.menu.img_background_button.height / 2) + 77 * scale_factor_height};
-	data->graphics.menu.play_button.size = (t_vec2i){data->graphics.menu.play_button.img.width, data->graphics.menu.play_button.img.height};
-
-
-	data->graphics.menu.settings_button.pos = (t_vec2i){data->graphics.menu.play_button.pos.x, data->graphics.menu.play_button.pos.y + data->graphics.menu.play_button.size.y + 14 * scale_factor_height};
-	data->graphics.menu.settings_button.size = (t_vec2i){data->graphics.menu.settings_button.img.width, data->graphics.menu.settings_button.img.height};
-
-	data->graphics.menu.exit_button.pos = (t_vec2i){data->graphics.menu.settings_button.pos.x, data->graphics.menu.settings_button.pos.y + data->graphics.menu.settings_button.size.y + 14 * scale_factor_height};
-	data->graphics.menu.exit_button.size = (t_vec2i){data->graphics.menu.exit_button.img.width, data->graphics.menu.exit_button.img.height};
-
-	data->graphics.menu.play_button.is_hovered = false;
-	data->graphics.menu.settings_button.is_hovered = false;
-	data->graphics.menu.exit_button.is_hovered = false;
-	data->graphics.menu.play_button.is_pressed = false;
-	data->graphics.menu.settings_button.is_pressed = false;
-	data->graphics.menu.exit_button.is_pressed = false;
+	scale_factor.x = (float)WIN_WIDTH / 1920.0f;
+	scale_factor.y = (float)WIN_HEIGHT / 1080.0f;
+	m = &data->graphics.menu;
+	load_menu_image(data, m);
+	resize_menu_image(data, m, scale_factor);
+	set_button_pos(m, scale_factor);
+	m->play_button.is_hovered = false;
+	m->settings_button.is_hovered = false;
+	m->exit_button.is_hovered = false;
+	m->play_button.is_pressed = false;
+	m->settings_button.is_pressed = false;
+	m->exit_button.is_pressed = false;
 }
